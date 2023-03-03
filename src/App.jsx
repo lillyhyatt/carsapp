5fbc4dda11b934f6aaae8cb5a70f11f23f7a6ad9
Lilly Hyatt <lillyhyatt@Lillys-MacBook-Pro.local>
2023-03-03 20:05:12 +0000

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
