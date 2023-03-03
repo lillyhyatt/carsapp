0719e0eb5341bca12da206ffe3c82f6a74d3ecc6
Lilly Hyatt <lillyhyatt@Lillys-MacBook-Pro.local>
2023-03-03 20:18:38 +0000

    router installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
-
+import React, { useState } from 'react'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+//Import pages
+import List from './pages/List'
+import Add from './pages/Add'
+import Update from './pages/Update'
+import NotFound from './pages/NotFound'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-
-    </div>
+<Router>
+<Routes>
+<Route path="/" element={<List />} />
+<Route path="/add" element={<Add />} />
+<Route path="/update" element={<Update />} />
+<Route path="*" element={<NotFound />} />
+</Routes>
+</Router>
   )
 }
 
