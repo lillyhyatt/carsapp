e65bc3f68363ecbd8976098c5a0084a1c12f4a6a
Lilly Hyatt <lillyhyatt@Lillys-MacBook-Pro.local>
2023-03-04 21:21:32 +0000

    Ui navigation added

---
+++ src/components/DesktopNavigation.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { useTheme } from '@mui/material/styles';
+
+
+
+function DesktopNavigation( { handleDrawerToggle=() => console.log ('no handleDrawToggle function'),}) {
+
+    const theme = useTheme();
+const lightTextColor = theme.palette.common.white;
+
+  return (
+    <>
+    
+        <AppBar component="nav">
+    <Toolbar>
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        edge="start"
+        onClick={handleDrawerToggle}
+        sx={{ mr: 2, display: { sm: 'none' } }}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Typography
+        variant="h6"
+        component={NavLink}
+        to="/"
+        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, textDecoration: 'none', color: lightTextColor}}
+      >
+       ToDo App
+      </Typography>
+      <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+<Button sx={{ color: '#fff' }} component={NavLink} to="/add">
+
+Add To do
+          </Button>
+          
+      </Box>
+    </Toolbar>
+  </AppBar>
+
+  <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+      </Box>
+
+    </>
+  )
+}
+
+export default DesktopNavigation;
---
+++ src/components/MobileNavigation.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
+
+
+function MobileNavigation({mobileOpen=false, handleDrawerToggle=() => console.log ('no handleDrawToggle function'), drawerWidth=240}) {
+  return (
+    <Box component="nav">
+    <Drawer
+      variant="temporary"
+      open={mobileOpen}
+      onClose={handleDrawerToggle}
+      ModalProps={{
+        keepMounted: true, // Better open performance on mobile.
+      }}
+      sx={{
+        display: { xs: 'block', sm: 'none' },
+        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+      }}
+    >
+         <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        ToDo App
+      </Typography>
+      <Divider />
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }} component={NavLink} to="/">
+              <ListItemText primary={"Home"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem  disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }} component={NavLink} to="/add">
+              <ListItemText primary={"Add Todo"} />
+            </ListItemButton>
+          </ListItem>
+        
+      </List>
+    </Box>
+    </Drawer>
+  </Box>
+  )
+}
+
+export default MobileNavigation
+
